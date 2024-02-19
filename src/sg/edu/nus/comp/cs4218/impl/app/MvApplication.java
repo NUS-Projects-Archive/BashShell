dbd9be9124431bf4d1716db7e30e83389f010478
JrmCkh <Hhelloogkart@gmail.com>
2024-02-19 15:05:50 +0800

    Add skeleton code for run

--- src/sg/edu/nus/comp/cs4218/impl/app/MvApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/MvApplication.java
@@ -1,16 +1,49 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import sg.edu.nus.comp.cs4218.app.MvInterface;
+import sg.edu.nus.comp.cs4218.exception.InvalidArgsException;
 import sg.edu.nus.comp.cs4218.exception.MvException;
+import sg.edu.nus.comp.cs4218.impl.parser.MvArgsParser;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
 
 public class MvApplication implements MvInterface {
 
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws MvException {
+        // Format: mv [Option] SOURCE TARGET
+        //         mv [Option] SOURCE … DIRECTORY
 
+        if (args == null || args.length == 0) {
+            throw new MvException(ERR_NULL_ARGS);
+        }
+
+        if (args.length < 2) {
+            throw new MvException(ERR_NO_ARGS);
+        }
+
+        // Parse argument(s) provided
+        final MvArgsParser parser = new MvArgsParser();
+        try {
+            parser.parse(args);
+        } catch (InvalidArgsException e) {
+            throw new MvException(e.getMessage());
+        }
+
+        final Boolean isOverwrite = parser.isOverwrite();
+        final String[] sourceDirectories = parser.getSourceDirectories()
+                .toArray(new String[parser.getSourceDirectories().size()]);
+        final String destinationDirectory = parser.getDestinationDirectory();
+
+        if (sourceDirectories.length > 1) {
+            mvFilesToFolder(isOverwrite, destinationDirectory, sourceDirectories);
+        } else {
+            mvSrcFileToDestFile(isOverwrite, sourceDirectories[0], destinationDirectory);
+        }
     }
 
     @Override
