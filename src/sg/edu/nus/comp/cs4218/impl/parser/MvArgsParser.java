d1260ba512b61057f9c264c6af319d87a9458333
JrmCkh <Hhelloogkart@gmail.com>
2024-02-19 13:55:14 +0800

    Add javadocs for MvArgsParser

--- src/sg/edu/nus/comp/cs4218/impl/parser/MvArgsParser.java
+++ src/sg/edu/nus/comp/cs4218/impl/parser/MvArgsParser.java
@@ -15,10 +15,20 @@ public class MvArgsParser extends ArgsParser {
         return flags.contains(FLAG_NO_OVERWRITE);
     }
 
+    /**
+     * Returns a list of source directories to be moved to the destination directory.
+     *
+     * @return A list of string representing the source directories.
+     */
     public List<String> getSourceDirectories() {
         return nonFlagArgs.subList(0, nonFlagArgs.size() - 1);
     }
 
+    /**
+     * Returns the destination directory where the source directories will be moved.
+     *
+     * @return A string representing the destination directory.
+     */
     public String getDestinationDirectory() {
         return nonFlagArgs.get(nonFlagArgs.size() - 1);
     }
