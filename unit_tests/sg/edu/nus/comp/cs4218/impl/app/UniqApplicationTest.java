7e261f021d99503025fff8331a593edc8f940c4f
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-02-22 12:29:41 +0800

    Refactor When section of unit tests
    Resolve some PMD issues.

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/UniqApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/UniqApplicationTest.java
@@ -1,8 +1,8 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 import java.io.ByteArrayOutputStream;
@@ -13,123 +13,94 @@ import sg.edu.nus.comp.cs4218.exception.UniqException;
 
 class UniqApplicationTest {
 
+    static final String TEST_FILE_ONE = "resources/uniq-1.txt";
+    static final String STR_HELLO_WORLD = "Hello World";
+    static final String STR_ALICE = "Alice";
+    static final String STR_BOB = "Bob";
+
     @Test
     void run_removeAdjacentDuplicates_onlyUniqueAdjacent() {
         //Given
-        String[] args = { "resources/uniq-1.txt" };
-        String expected =
-                "Hello World" + STRING_NEWLINE +
-                "Alice" + STRING_NEWLINE +
-                "Bob" + STRING_NEWLINE +
-                "Alice" + STRING_NEWLINE +
-                "Bob" + STRING_NEWLINE;
+        final String[] args = {TEST_FILE_ONE};
+        final String expected = STR_HELLO_WORLD + STRING_NEWLINE +
+                STR_ALICE + STRING_NEWLINE +
+                STR_BOB + STRING_NEWLINE +
+                STR_ALICE + STRING_NEWLINE +
+                STR_BOB + STRING_NEWLINE;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
+        UniqApplication app = new UniqApplication();
 
-        // When
-        try {
-            new UniqApplication().run(args, null, out);
-        } catch (Exception e) {
-            fail("Unexpected Error: " + e.getMessage());
-        }
-
-        // Then
-        assertEquals(expected, out.toString());
+        assertDoesNotThrow(() -> app.run(args, null, out)); // When
+        assertEquals(expected, out.toString()); // Then
     }
 
     @Test
     void run_countOccurrences_correctCounts() {
         //Given
-        String[] args = { "-c", "resources/uniq-1.txt" };
-        String expected =
-                "2 Hello World" + STRING_NEWLINE +
+        final String[] args = {"-c", TEST_FILE_ONE};
+        final String expected = "2 Hello World" + STRING_NEWLINE +
                 "2 Alice" + STRING_NEWLINE +
                 "1 Bob" + STRING_NEWLINE +
                 "1 Alice" + STRING_NEWLINE +
                 "1 Bob" + STRING_NEWLINE;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
+        UniqApplication app = new UniqApplication();
 
-        // When
-        try {
-            new UniqApplication().run(args, null, out);
-        } catch (Exception e) {
-            fail("Unexpected Error: " + e.getMessage());
-        }
-
-        // Then
-        assertEquals(expected, out.toString());
+        assertDoesNotThrow(() -> app.run(args, null, out)); // When
+        assertEquals(expected, out.toString()); // Then
     }
 
     @Test
     void run_duplicatesPerGroup_onlyOnePerDuplicateGroup() {
         //Given
-        String[] args = { "-d", "resources/uniq-1.txt" };
-        String expected =
-                "Hello World" + STRING_NEWLINE +
+        final String[] args = {"-d", TEST_FILE_ONE};
+        final String expected = "Hello World" + STRING_NEWLINE +
                 "Alice" + STRING_NEWLINE;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
+        UniqApplication app = new UniqApplication();
 
-        // When
-        try {
-            new UniqApplication().run(args, null, out);
-        } catch (Exception e) {
-            fail("Unexpected Error: " + e.getMessage());
-        }
-
-        // Then
-        assertEquals(expected, out.toString());
+        assertDoesNotThrow(() -> app.run(args, null, out)); // When
+        assertEquals(expected, out.toString()); // Then
     }
 
     @Test
     void run_AllDuplicates_AllDuplicateLines() {
         //Given
-        String[] args = { "-D", "resources/uniq-1.txt" };
-        String expected =
-                "Hello World" + STRING_NEWLINE +
+        final String[] args = {"-D", TEST_FILE_ONE};
+        final String expected = "Hello World" + STRING_NEWLINE +
                 "Hello World" + STRING_NEWLINE +
                 "Alice" + STRING_NEWLINE +
                 "Alice" + STRING_NEWLINE;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
+        UniqApplication app = new UniqApplication();
 
-        // When
-        try {
-            new UniqApplication().run(args, null, out);
-        } catch (Exception e) {
-            fail("Unexpected Error: " + e.getMessage());
-        }
-
-        // Then
-        assertEquals(expected, out.toString());
+        assertDoesNotThrow(() -> app.run(args, null, out)); // When
+        assertEquals(expected, out.toString()); // Then
     }
 
     @Test
     void run_countAndGroupDuplicate_correctCountAndLines() {
         //Given
-        String[] args = { "-cd", "resources/uniq-1.txt" };
-        String expected =
-                "2 Hello World" + STRING_NEWLINE +
+        final String[] args = {"-cd", TEST_FILE_ONE};
+        final String expected = "2 Hello World" + STRING_NEWLINE +
                 "2 Alice" + STRING_NEWLINE;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
+        UniqApplication app = new UniqApplication();
 
-        // When
-        try {
-            new UniqApplication().run(args, null, out);
-        } catch (Exception e) {
-            fail("Unexpected Error: " + e.getMessage());
-        }
-
-        // Then
-        assertEquals(expected, out.toString());
+        assertDoesNotThrow(() -> app.run(args, null, out)); // When
+        assertEquals(expected, out.toString()); // Then
     }
 
     @Test
     void run_countAndAllDuplicates_throwsUniqException() {
         //Given
-        String[] args = { "-cD", "resources/uniq-1.txt" };
-        String expectedMessage = "uniq: printing all duplicated lines and repeat counts is meaningless";
+        final String[] args = {"-cD", TEST_FILE_ONE};
+        final String expectedMessage = "uniq: printing all duplicated lines and repeat counts is meaningless";
+        UniqApplication app = new UniqApplication();
 
         // When
         UniqException exception = assertThrows(UniqException.class, () -> {
-            new UniqApplication().run(args, null, null);
+            app.run(args, null, null);
         });
 
         // Then
