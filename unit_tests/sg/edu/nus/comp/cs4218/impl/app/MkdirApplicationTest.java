1df2fca0ec6b1ffd4f19370d062c2ae3c73e7dbf
JrmCkh <Hhelloogkart@gmail.com>
2024-02-25 12:01:04 +0800

    Add stub for MkdirArgsParser

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
@@ -1,7 +1,13 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class MkdirApplicationTest {
 
+    private MkdirApplication app;
+
+    @BeforeEach
+    void setUp() {
+        this.app = new MkdirApplication();
+    }
 }
