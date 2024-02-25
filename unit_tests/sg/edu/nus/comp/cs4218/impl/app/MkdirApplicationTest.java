c0d31d195d924b7ad4b09e5c423d6c049e1c95ba
JrmCkh <Hhelloogkart@gmail.com>
2024-02-25 16:02:34 +0800

    Add Mkdir tests

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
@@ -1,13 +1,122 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.exception.MkdirException;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_EXISTS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_FOLDERS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_TOP_LEVEL_MISSING;
 
 class MkdirApplicationTest {
+
+    private final String MKDIR_EXCEPTION_MSG = "mkdir: ";
+    private final String EXISTING_FILE = "existingFile";
+    private final String NON_EXISTING_FILE = "nonExistingFile";
+    private final String NON_EXISTING_FILE_AND_TOP_LEVEL = "nonExistingTopLevel/nonExistingFile";
 
     private MkdirApplication app;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         this.app = new MkdirApplication();
+
+        File existingFile = new File(EXISTING_FILE);
+        existingFile.createNewFile();
+    }
+
+    @AfterEach
+    void tearDown() {
+        deleteIfExists(EXISTING_FILE);
+        deleteIfExists(NON_EXISTING_FILE);
+        deleteIfExists(NON_EXISTING_FILE_AND_TOP_LEVEL);
+    }
+
+    private void deleteIfExists(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            if (file.isFile()) {
+                file.delete();
+            } else {
+                // For directories, delete recursively
+                File parentDirectory = file.getParentFile();
+                if (parentDirectory != null) {
+                    deleteIfExists(parentDirectory.getPath());
+                }
+            }
+        }
+    }
+
+    @Test
+    void run_EmptyArgs_ThrowsNullArguments() {
+        Throwable result = assertThrows(MkdirException.class, () -> {
+            app.run(null, null, null);
+        });
+        assertEquals(MKDIR_EXCEPTION_MSG + ERR_NULL_ARGS, result.getMessage());
+    }
+
+    @Test
+    void run_NoDirectoriesArgs_ThrowsNoFolderNamesSupplied() {
+        String[] args = {"-p"};
+        Throwable result = assertThrows(MkdirException.class, () -> {
+            app.run(args, null, null);
+        });
+        assertEquals(MKDIR_EXCEPTION_MSG + ERR_NO_FOLDERS, result.getMessage());
+    }
+
+    @Test
+    void run_MissingTopLevel_ThrowsTopLevelMissing() {
+        String[] args = {"missingTopLevel/" + NON_EXISTING_FILE};
+        Throwable result = assertThrows(MkdirException.class, () -> {
+            app.run(args, null, null);
+        });
+        assertEquals(MKDIR_EXCEPTION_MSG + ERR_TOP_LEVEL_MISSING, result.getMessage());
+    }
+
+    @Test
+    void run_RootDirectory_ThrowsTopLevelMissing() {
+        String[] args = {"-p", "/"};
+        Throwable result = assertThrows(MkdirException.class, () -> {
+            app.run(args, null, null);
+        });
+        assertEquals(MKDIR_EXCEPTION_MSG + ERR_TOP_LEVEL_MISSING, result.getMessage());
+    }
+
+    @Test
+    void run_FolderDoNotExists_CreateFolderSuccessfully() throws MkdirException {
+        String[] args = {NON_EXISTING_FILE};
+        app.run(args, null, null);
+        File file = new File(NON_EXISTING_FILE);
+        assertTrue(file.exists());
+    }
+
+    @Test
+    void createFolder_FolderExists_ThrowsFileAlreadyExists() {
+        Throwable result = assertThrows(MkdirException.class, () -> {
+            app.createFolder(EXISTING_FILE);
+        });
+        assertEquals(MKDIR_EXCEPTION_MSG + ERR_FILE_EXISTS, result.getMessage());
+    }
+
+    @Test
+    void createFolder_FolderDoNotExists_CreateFolderSuccessfully() throws MkdirException {
+        File file = new File(NON_EXISTING_FILE);
+        app.createFolder(NON_EXISTING_FILE);
+        assertTrue(file.exists());
+    }
+
+    @Test
+    void createFolder_FolderAndTopLevelDoNotExist_CreateFolderSuccessfully() throws MkdirException {
+        File file = new File(NON_EXISTING_FILE_AND_TOP_LEVEL);
+        app.createFolder(NON_EXISTING_FILE_AND_TOP_LEVEL);
+        assertTrue(file.exists());
     }
 }
--- unit_tests/sg/edu/nus/comp/cs4218/impl/parser/MkdirArgsParserTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/parser/MkdirArgsParserTest.java
@@ -17,11 +17,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static sg.edu.nus.comp.cs4218.impl.parser.ArgsParser.ILLEGAL_FLAG_MSG;
 
 class MkdirArgsParserTest {
 
-    public static final String ILLEGAL_FLAG_MSG = "illegal option -- ";
-    private static final Set<Character> VALID_FLAGS = Set.of('p');
+    private final Set<Character> VALID_FLAGS = Set.of('p');
     private MkdirArgsParser mkdirArgsParser;
 
     private static Stream<Arguments> validSyntax() {
@@ -48,14 +48,14 @@ class MkdirArgsParserTest {
     }
 
     @Test
-    void parse_ValidFlag_ShouldReturnGivenMatchingFlag() throws InvalidArgsException {
+    void parse_ValidFlag_ReturnsGivenMatchingFlag() throws InvalidArgsException {
         mkdirArgsParser.parse("-p");
         assertEquals(VALID_FLAGS, mkdirArgsParser.flags, "Flags do not match");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"-a", "-1", "-!", "-P", "--"})
-    void parse_InvalidFlag_ShouldThrowInvalidArgsException(String args) {
+    void parse_InvalidFlag_ThrowsInvalidArgsException(String args) {
         Throwable result = assertThrows(InvalidArgsException.class, () -> {
             mkdirArgsParser.parse(args);
         });
@@ -64,49 +64,49 @@ class MkdirArgsParserTest {
 
     @ParameterizedTest
     @MethodSource("validSyntax")
-    void parse_validSyntax_ShouldNotThrowException(String... args) {
+    void parse_validSyntax_DoNotThrowException(String... args) {
         assertDoesNotThrow(() -> mkdirArgsParser.parse(args));
     }
 
     @ParameterizedTest
     @MethodSource("invalidSyntax")
-    void parse_invalidSyntax_ShouldThrowInvalidArgsException(String... args) {
+    void parse_invalidSyntax_ThrowsInvalidArgsException(String... args) {
         assertThrows(InvalidArgsException.class, () -> mkdirArgsParser.parse(args));
     }
 
     @Test
-    void isCreateParent_NoFlag_ShouldReturnFalse() throws InvalidArgsException {
+    void isCreateParent_NoFlag_ReturnsFalse() throws InvalidArgsException {
         mkdirArgsParser.parse();
         assertFalse(mkdirArgsParser.isCreateParent());
     }
 
     @Test
-    void isCreateParent_ValidFlag_ShouldReturnTrue() throws InvalidArgsException {
+    void isCreateParent_ValidFlag_ReturnsTrue() throws InvalidArgsException {
         mkdirArgsParser.parse("-p");
         assertTrue(mkdirArgsParser.isCreateParent());
     }
 
     @Test
-    void isCreateParent_ValidFlagAndNonFlagArg_ShouldReturnTrue() throws InvalidArgsException {
+    void isCreateParent_ValidFlagAndNonFlagArg_ReturnsTrue() throws InvalidArgsException {
         mkdirArgsParser.parse("-p", "example");
         assertTrue(mkdirArgsParser.isCreateParent());
     }
 
     @Test
-    void isCreateParent_OnlyNonFlagArg_ShouldReturnFalse() throws InvalidArgsException {
+    void isCreateParent_OnlyNonFlagArg_ReturnsFalse() throws InvalidArgsException {
         mkdirArgsParser.parse("example");
         assertFalse(mkdirArgsParser.isCreateParent());
     }
 
     @Test
-    void getDirectories_NoArg_ShouldReturnEmpty() throws InvalidArgsException {
+    void getDirectories_NoArg_ReturnsEmpty() throws InvalidArgsException {
         mkdirArgsParser.parse();
         List<String> result = mkdirArgsParser.getDirectories();
         assertTrue(result.isEmpty());
     }
 
     @Test
-    void getDirectories_OneNonFlagArg_ShouldReturnOneFolder() throws InvalidArgsException {
+    void getDirectories_OneNonFlagArg_ReturnsOneFolder() throws InvalidArgsException {
         mkdirArgsParser.parse("example");
         List<String> expected = List.of("example");
         List<String> result = mkdirArgsParser.getDirectories();
@@ -114,7 +114,7 @@ class MkdirArgsParserTest {
     }
 
     @Test
-    void getDirectories_MultipleNonFlagArg_ShouldReturnMultipleFolder() throws InvalidArgsException {
+    void getDirectories_MultipleNonFlagArg_ReturnsMultipleFolder() throws InvalidArgsException {
         mkdirArgsParser.parse("example1", "example2", "example3");
         List<String> expected = List.of("example1", "example2", "example3");
         List<String> result = mkdirArgsParser.getDirectories();
@@ -122,7 +122,7 @@ class MkdirArgsParserTest {
     }
 
     @Test
-    void getDirectories_ValidFlagAndOneNonFlagArg_ShouldReturnOneFolder() throws InvalidArgsException {
+    void getDirectories_ValidFlagAndOneNonFlagArg_ReturnsOneFolder() throws InvalidArgsException {
         mkdirArgsParser.parse("-p", "example");
         List<String> expected = List.of("example");
         List<String> result = mkdirArgsParser.getDirectories();
