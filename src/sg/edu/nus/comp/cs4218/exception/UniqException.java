92acb1813b22a69c031713af760bdc220abfb967
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-02-26 13:57:18 +0800

    Resolve some PMD issues

--- src/sg/edu/nus/comp/cs4218/exception/UniqException.java
+++ src/sg/edu/nus/comp/cs4218/exception/UniqException.java
@@ -2,14 +2,19 @@ package sg.edu.nus.comp.cs4218.exception;
 
 public class UniqException extends AbstractApplicationException {
 
-    //private static final long serialVersionUID = ???; //FIXME
+    private static final long serialVersionUID = -5428580805384267184L;
     public static final String INVALID_CMD = "Invalid command code.";
     public static final String PROB_UNIQ_FILE = "Problem matching unique from file: ";
     public static final String PROB_UNIQ_STDIN = "Problem matching unique from stdin: ";
-    public static final String MEANINGLESS_COUNT_ALL_DUP = "printing all duplicated lines and repeat " +
+    public static final String NO_POINT_COUNT_ALL_DUP = "printing all duplicated lines and repeat " +
             "counts is meaningless";
+
 
     public UniqException(final String message) {
         super("uniq: " + message);
+    }
+
+    public UniqException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
