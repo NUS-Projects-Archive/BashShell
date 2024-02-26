4a3026839301ee479df22e42b08dbeed440e404d
JrmCkh <Hhelloogkart@gmail.com>
2024-02-26 20:35:25 +0800

    Add test cases for sort app

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationTest.java
@@ -1,23 +1,27 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.io.TempDir;
 import sg.edu.nus.comp.cs4218.exception.SortException;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.Collections;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static sg.edu.nus.comp.cs4218.exception.SortException.PROB_SORT_FILE;
+import static sg.edu.nus.comp.cs4218.exception.SortException.PROB_SORT_STDIN;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_FILE_NOT_FOUND;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_IS_DIR;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_PERM;
@@ -26,56 +30,24 @@ import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_STREAMS;
 
 class SortApplicationTest {
 
-    private final String SORT_EXCEPTION_MSG = "sort: ";
-    private final String PROB_SORT_FILE = "Problem sort from file: ";
-    private final String PROB_SORT_STDIN = "Problem sort from stdin: ";
-    private final String EXISTING_FILE = "existingFile";
-    private final String EXISTING_DIRECTORY = "existingParent/existingChild/existingGrandchild";
-    private final String NON_EXISTING_FILE = "nonExistingFile";
-
+    private static final String SORT_EXCEPTION_MSG = "sort: ";
+    private final String TEMP_FILE = "file.txt";
     private SortApplication app;
+    @TempDir
+    private Path tempDir;
+    private Path tempFilePath;
 
-    private void createFile(String fileName) throws IOException {
-        File file = new File(fileName);
-        file.createNewFile();
-    }
-
-    private void createDirectory(String directoryPath) {
-        File directory = new File(directoryPath);
-        directory.mkdirs();
-    }
-
-    private void deleteFile(String filePath) {
-        File file = new File(filePath);
-        if (!file.exists()) {
-            return;
-        }
-        if (file.isDirectory()) {
-            deleteDirectory(file);
-        } else {
-            file.delete();
-        }
-    }
-
-    private void deleteDirectory(File directory) {
-        if (directory != null && directory.exists()) {
-            directory.delete();
-            deleteDirectory(directory.getParentFile());
-        }
+    private String joinStringsBySystemLineSeparator(String... strings) {
+        return String.join(System.lineSeparator(), strings);
     }
 
     @BeforeEach
     void setUp() throws IOException {
         this.app = new SortApplication();
-        createFile(EXISTING_FILE);
-        createDirectory(EXISTING_DIRECTORY);
-    }
 
-    @AfterEach
-    void tearDown() {
-        deleteFile(EXISTING_FILE);
-        deleteFile(EXISTING_DIRECTORY);
-        deleteFile(NON_EXISTING_FILE);
+        // Create temporary file
+        tempFilePath = tempDir.resolve(TEMP_FILE); // automatically deletes after test execution
+        Files.createFile(tempFilePath);
     }
 
     @Test
@@ -87,17 +59,74 @@ class SortApplicationTest {
     }
 
     @Test
+    void run_NoFlags_WritesSortedListToStdout() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        Files.write(tempFilePath, content.getBytes());
+        String[] args = {tempFilePath.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        String expected = joinStringsBySystemLineSeparator("A", "a", "b", "c") + System.lineSeparator();
+        app.run(args, null, stdout);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    void run_IsFirstWordNumberFlag_WritesSortedListToStdout() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("10", "1", "2");
+        Files.write(tempFilePath, content.getBytes());
+        String[] args = {"-n", tempFilePath.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        String expected = joinStringsBySystemLineSeparator("1", "2", "10") + System.lineSeparator();
+        app.run(args, null, stdout);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    void run_IsReverseOrderFlag_WritesReverseSortedListToStdout() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b");
+        Files.write(tempFilePath, content.getBytes());
+        String[] args = {"-r", tempFilePath.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        String expected = joinStringsBySystemLineSeparator("c", "b", "a") + System.lineSeparator();
+        app.run(args, null, stdout);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    void run_IsCaseIndependentFlag_WritesCaseIndependentSortedListToStdout() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        Files.write(tempFilePath, content.getBytes());
+        String[] args = {"-f", tempFilePath.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        String expected = joinStringsBySystemLineSeparator("a", "A", "b", "c") + System.lineSeparator();
+        app.run(args, null, stdout);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    void run_IsFirstWordNumberAndIsCaseIndependentFlag_IsFirstWordNumberTakesPrecedence()
+            throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "3", "B", "2", "C", "1");
+        Files.write(tempFilePath, content.getBytes());
+        String[] args = {"-n", "-f", tempFilePath.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        String expected = joinStringsBySystemLineSeparator("1", "2", "3", "B", "C", "a") + System.lineSeparator();
+        app.run(args, null, stdout);
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
     void sortFromFiles_EmptyFiles_ThrowsSortException() {
         Throwable result = assertThrows(SortException.class, () -> {
-            app.sortFromFiles(true, true, true, null);
+            app.sortFromFiles(false, false, false, null);
         });
         assertEquals(SORT_EXCEPTION_MSG + PROB_SORT_FILE + ERR_NULL_ARGS, result.getMessage());
     }
 
     @Test
     void sortFromFiles_FileDoNotExist_ThrowsSortException() {
+        Path nonExistFilePath = tempDir.resolve("nonExistFile.txt");
         Throwable result = assertThrows(SortException.class, () -> {
-            app.sortFromFiles(true, true, true, NON_EXISTING_FILE);
+            app.sortFromFiles(false, false, false, nonExistFilePath.toString());
         });
         assertEquals(SORT_EXCEPTION_MSG + PROB_SORT_FILE + ERR_FILE_NOT_FOUND, result.getMessage());
     }
@@ -105,7 +134,7 @@ class SortApplicationTest {
     @Test
     void sortFromFiles_FileGivenAsDirectory_ThrowsSortException() {
         Throwable result = assertThrows(SortException.class, () -> {
-            app.sortFromFiles(true, true, true, EXISTING_DIRECTORY);
+            app.sortFromFiles(false, false, false, tempDir.toString());
         });
         assertEquals(SORT_EXCEPTION_MSG + PROB_SORT_FILE + ERR_IS_DIR, result.getMessage());
     }
@@ -113,11 +142,90 @@ class SortApplicationTest {
     @Test
     @DisabledOnOs(value = OS.WINDOWS)
     void sortFromFiles_FileNoPermissionToRead_ThrowsSortException() throws IOException {
-        Path filePath = Paths.get(EXISTING_FILE);
-        Files.setPosixFilePermissions(filePath, new HashSet<>(Collections.singleton(PosixFilePermission.OWNER_READ)));
+        Files.setPosixFilePermissions(tempFilePath, new HashSet<>(Collections.singleton(PosixFilePermission.OWNER_READ)));
         Throwable result = assertThrows(SortException.class, () -> {
-            app.sortFromFiles(true, true, true, EXISTING_FILE);
+            app.sortFromFiles(false, false, false, TEMP_FILE);
         });
         assertEquals(SORT_EXCEPTION_MSG + PROB_SORT_FILE + ERR_NO_PERM, result.getMessage());
+    }
+
+    @Test
+    void sortFromFiles_NoFlags_ReturnsSortedList() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        Files.write(tempFilePath, content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("A", "a", "b", "c");
+        String result = app.sortFromFiles(false, false, false, tempFilePath.toString());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromFiles_IsFirstWordNumberFlag_ReturnsSortedList() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("10", "1", "2");
+        Files.write(tempFilePath, content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("1", "2", "10");
+        String result = app.sortFromFiles(true, false, false, tempFilePath.toString());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromFiles_IsReverseOrderFlag_ReturnsReverseSortedList() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b");
+        Files.write(tempFilePath, content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("c", "b", "a");
+        String result = app.sortFromFiles(false, true, false, tempFilePath.toString());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromFiles_IsCaseIndependentFlag_ReturnsCaseIndependentSortedList() throws SortException, IOException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        Files.write(tempFilePath, content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("a", "A", "b", "c");
+        String result = app.sortFromFiles(false, false, true, tempFilePath.toString());
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromStdin_NoStdin_ThrowsSortException() {
+        Throwable result = assertThrows(SortException.class, () -> {
+            app.sortFromStdin(false, false, false, null);
+        });
+        assertEquals(SORT_EXCEPTION_MSG + PROB_SORT_STDIN + ERR_NULL_STREAMS, result.getMessage());
+    }
+
+    @Test
+    void sortFromStdin_NoFlags_ReturnsSortedList() throws SortException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        InputStream stdin = new ByteArrayInputStream(content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("A", "a", "b", "c");
+        String result = app.sortFromStdin(false, false, false, stdin);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromStdin_IsFirstWordNumberFlag_ReturnsSortedList() throws SortException {
+        String content = joinStringsBySystemLineSeparator("10", "1", "2");
+        InputStream stdin = new ByteArrayInputStream(content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("1", "2", "10");
+        String result = app.sortFromStdin(true, false, false, stdin);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromStdin_IsReverseOrderFlag_ReturnsReverseSortedList() throws SortException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b");
+        InputStream stdin = new ByteArrayInputStream(content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("c", "b", "a");
+        String result = app.sortFromStdin(false, true, false, stdin);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void sortFromStdin_IsCaseIndependentFlag_ReturnsCaseIndependentSortedList() throws SortException {
+        String content = joinStringsBySystemLineSeparator("a", "c", "b", "A");
+        InputStream stdin = new ByteArrayInputStream(content.getBytes());
+        String expected = joinStringsBySystemLineSeparator("a", "A", "b", "c");
+        String result = app.sortFromStdin(false, false, true, stdin);
+        assertEquals(expected, result);
     }
 }
