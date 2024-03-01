6a25e097889338bb9d0fe06d8100d3de187a918a
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-01 14:29:34 +0800

    Add tests for RmArgsParser
    Passed PMD checks

---
+++ src/sg/edu/nus/comp/cs4218/skeleton/parser/RmArgsParser.java
@@ -0,0 +1,21 @@
+package sg.edu.nus.comp.cs4218.skeleton.parser;
+
+import sg.edu.nus.comp.cs4218.impl.parser.ArgsParser;
+
+public class RmArgsParser extends ArgsParser {
+
+    public RmArgsParser() {
+        super();
+        // FIXME: Implement as part of RmApplication
+    }
+
+    public boolean isRecursive() {
+        // FIXME: Implement as part of RmApplication
+        return true;
+    }
+
+    public boolean isEmptyDirectory() {
+        // FIXME: Implement as part of RmApplication
+        return true;
+    }
+}
