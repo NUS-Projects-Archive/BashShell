42a493eb756d002cbde57f389892d8e262528ba4
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-01 15:05:33 +0800

    Move run() tests from unit tests to integration test
    Suppressed PMD.ClassNamingConvention
    as PDF state integrations tests should follow *IT.java

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/UniqApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/UniqApplicationTest.java
@@ -1,12 +1,9 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.fail;
 import static sg.edu.nus.comp.cs4218.impl.util.AssertUtils.assertFileMatch;
-import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
@@ -17,8 +14,6 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -27,14 +22,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 import sg.edu.nus.comp.cs4218.exception.UniqException;
 
 class UniqApplicationTest {
-
     static final String TEST_RESOURCES = "resources/uniq/";
     static final String TEST_INPUT_FILE = TEST_RESOURCES + "input.txt";
     static final String TEST_OUTPUT_FILE = "test-output.txt";
-    static final String STR_HELLO_WORLD = "Hello World";
-    static final String STR_ALICE = "Alice";
-    static final String STR_BOB = "Bob";
-
     UniqApplication app;
     ByteArrayOutputStream outputStream;
 
@@ -65,93 +55,6 @@ class UniqApplicationTest {
     @AfterEach
     void tearDown() throws IOException {
         outputStream.close();
-    }
-
-    @Test
-    void run_NoFlags_OnlyUniqueAdjacentLines() {
-        //Given
-        final String[] args = {TEST_INPUT_FILE};
-        final String expected = STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE +
-                STR_BOB + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE +
-                STR_BOB + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    void run_CountFlag_CorrectOccurrenceCounts() {
-        //Given
-        final String[] args = {"-c", TEST_INPUT_FILE};
-        final String expected = "2 Hello World" + STRING_NEWLINE +
-                "2 Alice" + STRING_NEWLINE +
-                "1 Bob" + STRING_NEWLINE +
-                "1 Alice" + STRING_NEWLINE +
-                "1 Bob" + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    void run_GroupDuplicatesFlag_OnlyOnePerDuplicateGroup() {
-        //Given
-        final String[] args = {"-d", TEST_INPUT_FILE};
-        final String expected = STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    void run_AllDuplicatesFlag_AllDuplicateLines() {
-        //Given
-        final String[] args = {"-D", TEST_INPUT_FILE};
-        final String expected = STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    @DisplayName("Test -D flag's precedence over -d flag")
-    void run_GroupDuplicatesAndAllDuplicatesFlags_AllDuplicateLines() {
-        //Given
-        final String[] args = {"-dD", TEST_INPUT_FILE};
-        final String expected = STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_HELLO_WORLD + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE +
-                STR_ALICE + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    void run_CountAndGroupDuplicateFlags_CorrectCountAndLines() {
-        //Given
-        final String[] args = {"-cd", TEST_INPUT_FILE};
-        final String expected = "2 Hello World" + STRING_NEWLINE +
-                "2 Alice" + STRING_NEWLINE;
-
-        assertDoesNotThrow(() -> app.run(args, null, outputStream)); // When
-        assertEquals(expected, outputStream.toString()); // Then
-    }
-
-    @Test
-    void run_CountAndAllDuplicatesFlags_ThrowsUniqException() {
-        //Given
-        final String[] args = {"-cD", TEST_INPUT_FILE};
-        final String expectedMessage = "uniq: printing all duplicated lines and repeat counts is meaningless";
-
-        Throwable thrown = assertThrows(UniqException.class, () -> app.run(args, null, null)); // When
-        assertEquals(expectedMessage, thrown.getMessage()); // Then
     }
 
     @ParameterizedTest
