80aad6eae0924019e656b38e8df77d79a5f8c034
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-01 14:51:52 +0800

    Move run() tests from unit tests to integration test
    Suppressed PMD.ClassNamingConvention
    as PDF state integrations tests should follow *IT.java

--- unimplemented_tests/sg/edu/nus/comp/cs4218/impl/app/RmApplicationIT.java
+++ unimplemented_tests/sg/edu/nus/comp/cs4218/impl/app/RmApplicationIT.java
@@ -21,7 +21,8 @@ import sg.edu.nus.comp.cs4218.exception.RmException;
 import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 import sg.edu.nus.comp.cs4218.skeleton.app.RmApplication;
 
-public class RmApplicationTest {
+@SuppressWarnings("PMD.ClassNamingConventions")
+public class RmApplicationIT {
     private static final String TEST_RESOURCES = "resources/rm/";
     private static final String EMPTY_DIRECTORY = "empty_directory";
     private static final String TEST_DIRECTORY = "test_folder";
@@ -57,51 +58,72 @@ public class RmApplicationTest {
     }
 
     @Test
-    void remove_ExistingFile_SuccessfullyRemoveFile() {
-        assertDoesNotThrow(() -> app.remove(false, false, TEST_FILE_ONE));
+    void run_NoArgs_ThrowsRmException() {
+        final String expectedMsg = "rm: missing operand"; // Given
+        RmException exception = assertThrowsExactly(RmException.class, () -> app.run(null, null, null)); // When
+        assertEquals(expectedMsg, exception.getMessage()); // Then
+    }
+
+    @Test
+    void run_RemoveExistingFile_SuccessfullyRemoveFile() {
+        final String[] args = {TEST_FILE_ONE};
+        assertDoesNotThrow(() -> app.run(args, null, null));
         assertTrue(Files.notExists(Paths.get(testingDirectory.toString(), TEST_FILE_ONE)));
     }
 
     @Test
-    void remove_NonExistentFile_ThrowsRmException() {
+    void run_RemoveNonExistentFile_ThrowsRmException() {
+        // Given
+        final String[] args = {NON_EXIST_FILE};
         final String expectedMsg = String.format("rm: cannot remove '%s': No such file or directory", NON_EXIST_FILE);
-        RmException exception = assertThrowsExactly(RmException.class, () ->
-                app.remove(false, false, NON_EXIST_FILE)
-        ); // When
+
+        RmException exception = assertThrowsExactly(RmException.class, () -> app.run(args, null, null)); // When
         assertEquals(expectedMsg, exception.getMessage()); // Then
     }
 
     @Test
-    void remove_DirectoryWithoutFlag_ThrowsRmException() {
+    void run_RemoveDirectoryWithoutFlag_ThrowsRmException() {
+        // Given
+        final String[] args = {TEST_DIRECTORY};
         final String expectedMsg = String.format("rm: cannot remove '%s': Is a directory", TEST_DIRECTORY);
-        RmException exception = assertThrowsExactly(RmException.class, () ->
-                app.remove(false, false, TEST_DIRECTORY)
-        ); // When
+
+        RmException exception = assertThrowsExactly(RmException.class, () -> app.run(args, null, null)); // When
         assertEquals(expectedMsg, exception.getMessage()); // Then
     }
 
     @Test
-    void remove_EmptyDirectory_SuccessfullyRemoveDirectory() {
-        assertDoesNotThrow(() -> app.remove(true, false, EMPTY_DIRECTORY));
+    void run_RemoveEmptyDirectory_SuccessfullyRemoveDirectory() {
+        final String[] args = {"-d", EMPTY_DIRECTORY};
+        assertDoesNotThrow(() -> app.run(args, null, null));
         assertTrue(Files.notExists(Paths.get(testingDirectory.toString(), EMPTY_DIRECTORY)));
     }
 
     @Test
-    void remove_NonEmptyDirectory_ThrowsRmException() {
+    void run_RemoveNonEmptyDirectory_ThrowsRmException() {
+        // Given
+        final String[] args = {"-d", TEST_DIRECTORY};
         final String expectedMsg = String.format("rm: cannot remove '%s': Directory not empty", TEST_DIRECTORY);
-        RmException exception = assertThrowsExactly(RmException.class, () ->
-                app.remove(true, false, TEST_DIRECTORY)
-        ); // When
+
+        RmException exception = assertThrowsExactly(RmException.class, () -> app.run(args, null, null)); // When
         assertEquals(expectedMsg, exception.getMessage()); // Then
     }
 
     @Test
-    void remove_SingleDirectoryRecursively_SuccessfullyRemoveAllFilesAndDirectory() {
-        assertDoesNotThrow(() -> app.remove(false, true, TEST_DIRECTORY));
+    void run_RemoveSingleDirectoryRecursively_SuccessfullyRemoveAllFilesAndDirectory() {
+        final String[] args = {"-r", TEST_DIRECTORY};
+        assertDoesNotThrow(() -> app.run(args, null, null));
         assertTrue(Files.exists(Paths.get(testingDirectory.toString(), TEST_FILE_ONE))); // not in folder, should exist
         assertTrue(Files.exists(Paths.get(testingDirectory.toString(), EMPTY_DIRECTORY))); // not in folder, should
         // exist
         assertTrue(Files.notExists(Paths.get(testingDirectory.toString(), TEST_DIRECTORY)));
     }
 
+    @Test
+    void run_RemoveMultipleDirectoriesRecursively_SuccessfullyRemoveAllFilesAndDirectory() {
+        final String[] args = {"-r", TEST_DIRECTORY, EMPTY_DIRECTORY};
+        assertDoesNotThrow(() -> app.run(args, null, null));
+        assertTrue(Files.exists(Paths.get(testingDirectory.toString(), TEST_FILE_ONE))); // not in folder, should exist
+        assertTrue(Files.notExists(Paths.get(testingDirectory.toString(), EMPTY_DIRECTORY)));
+        assertTrue(Files.notExists(Paths.get(testingDirectory.toString(), TEST_DIRECTORY)));
+    }
 }
