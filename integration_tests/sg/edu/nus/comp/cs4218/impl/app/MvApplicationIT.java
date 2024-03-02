5076db46a42a5888c86b524cb44f15e5bfb8092e
JrmCkh <Hhelloogkart@gmail.com>
2024-03-02 20:50:29 +0800

    Add 2 more run tests for Mv

--- integration_tests/sg/edu/nus/comp/cs4218/impl/app/MvApplicationIT.java
+++ integration_tests/sg/edu/nus/comp/cs4218/impl/app/MvApplicationIT.java
@@ -1,12 +1,18 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -49,5 +55,34 @@ public class MvApplicationIT {
             app.run(args.split("\\s+"), null, null);
         });
         assertEquals(expectedMsg, exception.getMessage());
+    }
+
+    @Test
+    void run_MovesSrcFiles_DoNotWriteToStdout(@TempDir Path tempDir) {
+        Path subDir = tempDir.resolve("subdirectory");
+        assertDoesNotThrow(() -> Files.createDirectories(subDir));
+        Path file = tempDir.resolve("file");
+        assertDoesNotThrow(() -> Files.createFile(file));
+
+        String[] args = {file.toString(), subDir.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> app.run(args, null, stdout));
+
+        String expected = "";
+        assertEquals(expected, stdout.toString());
+    }
+
+    @Test
+    void run_MovesMultipleDoNotExistSrcFile_WritesErrorToStdout(@TempDir Path tempDir) {
+        Path subDir = tempDir.resolve("subdirectory");
+        assertDoesNotThrow(() -> Files.createDirectories(subDir));
+
+        String[] args = {"a", "b", subDir.toString()};
+        OutputStream stdout = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> app.run(args, null, stdout));
+
+        String expected = "mv: Problem move to folder: 'a': No such file or directory" + System.lineSeparator() +
+                "mv: Problem move to folder: 'b': No such file or directory" + System.lineSeparator();
+        assertEquals(expected, stdout.toString());
     }
 }
