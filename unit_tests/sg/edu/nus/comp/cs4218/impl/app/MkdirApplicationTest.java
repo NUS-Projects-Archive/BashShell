7748486ac2a3db688830b1e8622ac6b824b699bb
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-16 22:13:26 +0800

    Fix bad merge

--- unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
+++ unit_tests/sg/edu/nus/comp/cs4218/impl/app/MkdirApplicationTest.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static sg.edu.nus.comp.cs4218.test.AssertUtils.assertFileExists;
 
 import java.io.IOException;
