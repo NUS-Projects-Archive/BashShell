dcbe416fabd20e1c52bdcbe823cebbf6eeae94ba
JrmCkh <Hhelloogkart@gmail.com>
2024-03-17 14:46:13 +0800

    Fix PMD violation

--- public_tests/sg/edu/nus/comp/cs4218/impl/app/CutApplicationPublicTest.java
+++ public_tests/sg/edu/nus/comp/cs4218/impl/app/CutApplicationPublicTest.java
@@ -1,11 +1,11 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static sg.edu.nus.comp.cs4218.testutils.TestStringUtils.STRING_NEWLINE;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -16,7 +16,13 @@ import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.exception.CutException;
 
 public class CutApplicationPublicTest {
-    CutApplication cutApplication;
+
+    private static final int[] RANGE_1_TO_3 = {1, 3};
+    private static final String[] HELLO_WORLD_ARRAY = {"hello", "world"};
+    private static final String HELLO_WORLD = "hello world";
+    private static final String HEL = "hel";
+    private static final String HEL_WOR = "hel" + STRING_NEWLINE + "wor";
+    private CutApplication cutApplication;
 
     private String joinStringsByLineSeparator(String... strs) {
         return String.join(STRING_NEWLINE, strs);
@@ -32,53 +38,52 @@ public class CutApplicationPublicTest {
     }
 
     @Test
-    void cutFromStdin_NullContent_ThrowsException() {
+    void cutFromStdin_NullContent_ThrowsCutException() {
         int[] ranges = new int[]{1, 2};
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
         assertThrows(CutException.class, () -> cutApplication.cutFromStdin(false, true, List.of(ranges), null));
     }
 
     @Test
-    void cutFromStdin_SingleLineByCharRange_ReturnCutByLine() throws Exception {
-        int[] ranges = new int[]{1, 3};
-        InputStream stdin = generateInputStreamFromStrings("hello world");
-        String actual = cutApplication.cutFromStdin(true, false, List.of(ranges), stdin);
-        assertEquals("hel", actual);
+    void cutFromStdin_SingleLineByCharRange_ReturnCutByLine() {
+        String actual = assertDoesNotThrow(() -> {
+            InputStream stdin = generateInputStreamFromStrings(HELLO_WORLD);
+            return cutApplication.cutFromStdin(true, false, List.of(RANGE_1_TO_3), stdin);
+        });
+        assertEquals(HEL, actual);
     }
 
     @Test
-    void cutFromStdin_SingleLineByByteRange_ReturnCutByByte() throws Exception {
-        int[] ranges = new int[]{1, 3};
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        InputStream stdin = generateInputStreamFromStrings("hello world");
-        String actual = cutApplication.cutFromStdin(false, true, List.of(ranges), stdin);
-        assertEquals("hel", actual);
+    void cutFromStdin_SingleLineByByteRange_ReturnCutByByte() {
+        String actual = assertDoesNotThrow(() -> {
+            InputStream stdin = generateInputStreamFromStrings(HELLO_WORLD);
+            return cutApplication.cutFromStdin(false, true, List.of(RANGE_1_TO_3), stdin);
+        });
+        assertEquals(HEL, actual);
     }
 
     @Test
-    void cutFromStdin_MultipleLinesByCharRange_ReturnCutContentAtEachLineByByte() throws Exception {
-        int[] ranges = new int[]{1, 3};
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        InputStream stdin = generateInputStreamFromStrings("hello", "world");
-        String actual = cutApplication.cutFromStdin(true, false, List.of(ranges), stdin);
-        assertEquals("hel" + STRING_NEWLINE + "wor", actual);
+    void cutFromStdin_MultipleLinesByCharRange_ReturnCutContentAtEachLineByByte() {
+        String actual = assertDoesNotThrow(() -> {
+            InputStream stdin = generateInputStreamFromStrings(HELLO_WORLD_ARRAY);
+            return cutApplication.cutFromStdin(true, false, List.of(RANGE_1_TO_3), stdin);
+        });
+        assertEquals(HEL_WOR, actual);
     }
 
     @Test
-    void cutFromStdin_MultipleLinesByByteRange_ReturnCutContentAtEachLineByByte() throws Exception {
-        int[] ranges = new int[]{1, 3};
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        InputStream stdin = generateInputStreamFromStrings("hello", "world");
-        String actual = cutApplication.cutFromStdin(false, true, List.of(ranges), stdin);
-        assertEquals("hel" + STRING_NEWLINE + "wor", actual);
+    void cutFromStdin_MultipleLinesByByteRange_ReturnCutContentAtEachLineByByte() {
+        String actual = assertDoesNotThrow(() -> {
+            InputStream stdin = generateInputStreamFromStrings(HELLO_WORLD_ARRAY);
+            return cutApplication.cutFromStdin(false, true, List.of(RANGE_1_TO_3), stdin);
+        });
+        assertEquals(HEL_WOR, actual);
     }
 
 
     @Test
-    void cutFromFile_InvalidFile_ThrowsException() {
-        int[] ranges = new int[]{1, 3};
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        assertThrows(CutException.class,
-                () -> cutApplication.cutFromFiles(false, true, List.of(ranges), "invalidFile"));
+    void cutFromFile_InvalidFile_ThrowsCutException() {
+        assertThrows(CutException.class, () ->
+                cutApplication.cutFromFiles(false, true, List.of(RANGE_1_TO_3), "invalidFile")
+        );
     }
 }
