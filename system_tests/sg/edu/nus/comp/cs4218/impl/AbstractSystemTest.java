d1c932bdd9838f3d522c67ee32cc674ede03022d
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-20 10:30:18 +0800

    Set up code to allow easy test case generation and testing

---
+++ system_tests/sg/edu/nus/comp/cs4218/impl/AbstractSystemTest.java
@@ -0,0 +1,74 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrNormalized;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOutNormalized;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.withTextFromSystemIn;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.io.CleanupMode.ALWAYS;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.io.TempDir;
+
+import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
+
+@SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
+public abstract class AbstractSystemTest {
+
+    static String rootDirectory = System.getProperty("user.dir");
+
+    /**
+     * Test {@code ShellImpl::main} using the given inputs.
+     *
+     * @param inputs Strings acting as stdin
+     * @return {@code SystemTestResults} of the test
+     */
+    static SystemTestResults testMainWith(String... inputs) {
+        SystemTestResults res = new SystemTestResults();
+        StringBuilder exitingDirectory = new StringBuilder();
+
+        assertDoesNotThrow(() -> {
+            res.exitCode = catchSystemExit(() -> {
+                res.err = tapSystemErrNormalized(() -> {
+                    res.out = tapSystemOutNormalized(() -> {
+                        withTextFromSystemIn(inputs).execute(() -> {
+                            Environment.currentDirectory = rootDirectory;
+                            res.rootDirectory = Environment.currentDirectory;
+                            ShellImpl.main();
+                            exitingDirectory.append(Environment.currentDirectory);
+                        });
+                    });
+                });
+            });
+        });
+
+        final String extraClosingLine = "\n" + exitingDirectory + "$ ";
+        res.out = StringUtils.removeTrailingOnce(res.out, extraClosingLine);
+        res.err = StringUtils.removeTrailingOnce(res.err, "null\n");
+
+        return res;
+    }
+
+    @BeforeEach
+    void beforeEach(@TempDir(cleanup = ALWAYS) Path tempDir) {
+        rootDirectory = tempDir.toAbsolutePath().toString();
+    }
+
+    /**
+     * Object holding out, err and exit code of a system test.
+     * Also holds the "root" directory of the test environment.
+     */
+    static class SystemTestResults {
+        String out;
+        String err;
+        String rootDirectory;
+        int exitCode;
+
+        String rootPath(String children) {
+            return rootDirectory + children + "$ ";
+        }
+    }
+}
