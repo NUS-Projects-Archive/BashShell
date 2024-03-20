266da29fbb63073d353b868b211c223f302adaae
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-03-20 10:21:50 +0800

    Add null checks for StringUtils::removeTrailing
    Add StringUtils::removeTrailingOnce.

--- src/sg/edu/nus/comp/cs4218/impl/util/StringUtils.java
+++ src/sg/edu/nus/comp/cs4218/impl/util/StringUtils.java
@@ -116,15 +116,41 @@ public final class StringUtils {
     }
 
     /**
-     * Removes trailing instances of given sequence.
+     * Recursively removes trailing instances of given sequence.
+     * Returns the original string if it does not end with the sequence.
      *
      * @param str      String to remove sequence from
      * @param sequence String of sequence to remove
      * @return String of result with trailing sequence removed
      */
     public static String removeTrailing(String str, String sequence) {
-        String result = str.substring(0, str.length() - sequence.length());
-        return str.endsWith(sequence) ? removeTrailing(result, sequence) : str;
+        if (str == null || sequence == null) {
+            return str;
+        }
+
+        final int strLength = str.length();
+        final int seqLength = sequence.length();
+        return str.endsWith(sequence)
+                ? removeTrailing(str.substring(0, strLength - seqLength), sequence)
+                : str;
+    }
+
+    /**
+     * Removes trailing instances of given sequence.
+     * Returns the original string if it does not end with the sequence.
+     *
+     * @param str      String to remove sequence from
+     * @param sequence String of sequence to remove
+     * @return String of result with trailing sequence removed
+     */
+    public static String removeTrailingOnce(String str, String sequence) {
+        if (str == null || sequence == null) {
+            return str;
+        }
+
+        final int strLength = str.length();
+        final int seqLength = sequence.length();
+        return str.endsWith(sequence) ? str.substring(0, strLength - seqLength) : str;
     }
 
 
