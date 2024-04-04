895ca924011bceafd813a9c497367fbfc999fa5c
JrmCkh <Hhelloogkart@gmail.com>
2024-04-04 17:35:07 +0800

    Update sort IT FileInputTests to use tempDir for resources

--- integration_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationIT.java
+++ integration_tests/sg/edu/nus/comp/cs4218/impl/app/SortApplicationIT.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
@@ -9,6 +10,7 @@ import static org.mockito.Mockito.mock;
 import static sg.edu.nus.comp.cs4218.testutils.FileUtils.createNewFile;
 import static sg.edu.nus.comp.cs4218.testutils.TestStringUtils.STRING_NEWLINE;
 import static sg.edu.nus.comp.cs4218.testutils.TestStringUtils.joinStringsByNewline;
+import static sg.edu.nus.comp.cs4218.testutils.TestStringUtils.removeTrailing;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,18 +20,24 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.CleanupMode;
+import org.junit.jupiter.api.io.TempDir;
 
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.SortException;
 
 @SuppressWarnings("PMD.ClassNamingConventions")
 public class SortApplicationIT {
 
     private static final String TEST_RESOURCES = "resources/sort/";
-    private static final String TEST_INPUT_FILE = TEST_RESOURCES + "large-text.txt";
+    private static final String TEST_INPUT_FILE = "large-text.txt";
+    private static final String TEST_OUT_NUMERIC = "out-numeric.txt";
+    private static final String TEST_OUT_NUM_REV = "out-numeric-reverse.txt";
     private static final String BIG_A = "A";
     private static final String BIG_B = "B";
     private static final String SMALL_A = "a";
@@ -43,7 +51,6 @@ public class SortApplicationIT {
     private static final String[] OUT_FIRST_NUM = {PLUS, MINUS, BIG_A, BIG_B, SMALL_A, SMALL_B, SMALL_O, "1", "2", "3", "5", "10"};
     private static final String[] OUT_REV_ORDER = {SMALL_O, SMALL_B, SMALL_A, BIG_B, BIG_A, "5", "3", "2", "10", "1", MINUS, PLUS};
     private static final String[] OUT_CASE_IGNORE = {PLUS, MINUS, "1", "10", "2", "3", "5", BIG_A, SMALL_A, BIG_B, SMALL_B, SMALL_O};
-
 
     private SortApplication app;
     private OutputStream stdout;
@@ -88,11 +95,31 @@ public class SortApplicationIT {
     @Nested
     class FileInputTests {
 
+        @TempDir
+        private Path testingDirectory;
         private InputStream mockStdin;
         private String file;
 
         @BeforeEach
-        void setUp() throws IOException {
+        void setUp(@TempDir(cleanup = CleanupMode.ALWAYS) Path tempDir) throws IOException {
+            testingDirectory = tempDir;
+            final String resourceDirectory = removeTrailing(TEST_RESOURCES, "/");
+            try (Stream<Path> stream = Files.walk(Paths.get(resourceDirectory))) {
+                stream.forEach(source -> {
+                    Path destination = Paths.get(testingDirectory.toString(),
+                            source.toString().substring(resourceDirectory.length()));
+
+                    try {
+                        Files.copy(source, destination, REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        throw new RuntimeException("Unable to copy test resources to temp directory.", e);
+                    }
+                });
+            }
+
+            // Set CWD to be the test directory
+            Environment.currentDirectory = testingDirectory.toString();
+
             mockStdin = mock(InputStream.class);
 
             // Create temporary file, automatically deletes after test execution
@@ -144,7 +171,7 @@ public class SortApplicationIT {
         void run_IsFirstWordNumberAndIsCaseIndependentFlagForLargeTextFile_IsFirstWordNumberTakesPrecedence() {
             String[] args = {"-f", "-n", TEST_INPUT_FILE};
             assertDoesNotThrow(() -> app.run(args, mockStdin, stdout));
-            Path expectedFilePath = Paths.get(TEST_RESOURCES + "out-numeric.txt");
+            Path expectedFilePath = Paths.get(testingDirectory.toString(), TEST_OUT_NUMERIC);
             String expectedContent = assertDoesNotThrow(() -> Files.readString(expectedFilePath));
             assertEquals(expectedContent, stdout.toString());
         }
@@ -153,7 +180,7 @@ public class SortApplicationIT {
         void run_IsFirstWordNumberAndIsReverseAndIsCaseIndependentFlagForLargeTextFile_IsFirstWordNumberTakesPrecedence() {
             String[] args = {"-fr", "-n", TEST_INPUT_FILE};
             assertDoesNotThrow(() -> app.run(args, mockStdin, stdout));
-            Path expectedFilePath = Paths.get(TEST_RESOURCES + "out-numeric-reverse.txt");
+            Path expectedFilePath = Paths.get(testingDirectory.toString(), TEST_OUT_NUM_REV);
             String expectedContent = assertDoesNotThrow(() -> Files.readString(expectedFilePath));
             assertEquals(expectedContent, stdout.toString());
         }
